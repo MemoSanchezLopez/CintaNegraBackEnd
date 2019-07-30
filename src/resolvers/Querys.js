6d05c6c83e2b302b681cdaee834aa2373cb2f5b9
GuillermoSanchezLopez <guillermo.sanchez.lopez93@gmail.com>
2019-07-29 21:51:12 -0500

    Comentarios
    Se suben comentarios de los Querys.

--- src/resolvers/Querys.js
+++ src/resolvers/Querys.js
@@ -1,10 +1,20 @@
 const Report = require('../models/report');
 
+/**
+ * Metodo para obtener los reportes por Categoria
+ * @param {*} root 
+ * @param {*} args 
+ */
 const reporteByCat = (root, args) => {
     let reports = Report.findOne({category: args.name}).populate('users');
     return reports;
 }
 
+/**
+ * Metodo para obtener todos los reportes
+ * @param {*} root 
+ * @param {*} args 
+ */
 const getAllReports = async (root, args) => {
     let reports = await Report.find();
     return reports;
